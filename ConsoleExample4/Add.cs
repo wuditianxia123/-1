97bfd409780531498e3864f2a8b7b7d78e878689
wuditianxia123 <75348279+wuditianxia123@users.noreply.github.com>
2021-03-28 21:51:06 +0800

    Add files via upload

---
+++ ConsoleExample4/Add.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleExample4
+{
+    class Add:JiSuan
+    {
+        string c;
+
+public string C
+{
+  get { return c; }
+  set { c = value; }
+}
+        string d;
+
+public string D
+{
+  get { return d; }
+  set { d = value; }
+}
+        
+      //  public string C { get => c; set => c = value; }
+       // public string D { get => d; set => d = value; }
+
+        public override void GetNum()
+        {
+            Console.WriteLine(c+ d);
+            Console.WriteLine(Num = A + B);
+        }
+    }
+}
---
+++ ConsoleExample4/JiSuan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleExample4
+{
+    class JiSuan
+
+    {
+        int a;
+
+public int A
+{
+  get { return a; }
+  set { a = value; }
+}
+        int b;
+
+public int B
+{
+  get { return b; }
+  set { b = value; }
+}
+int num;
+
+public int Num
+{
+    get { return num; }
+    set { num = value; }
+}
+
+       // public int A { get => a; set => a = value; }
+       // public int B { get => b; set => b = value; }
+      
+        //public int Num {  set => num = value; }
+
+        public virtual void GetNum()
+        {
+
+        }
+    }
+}
